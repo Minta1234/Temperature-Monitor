03b518837c323f16edc9316c867cd3e4b10d96b2
Minta1234 <kawinphopkodsombat@gmail.com>
2025-08-01 17:35:20 +0700

    Update microbit.py

--- microbit.py
+++ microbit.py
@@ -1,14 +1,17 @@
-
 temp = 0
+# เริ่มใช้งาน Bluetooth UART
 bluetooth.start_uart_service()
+# ตั้งค่า Serial USB สำหรับเชื่อมกับคอมพิวเตอร์
+serial.set_baud_rate(BaudRate.BAUD_RATE115200)
 
 def on_forever():
     global temp
     temp = input.temperature()
-  
-    serial.write_line("USB Temp:" + ("" + str(temp)))
-    bluetooth.uart_write_line("BLE Temp:" + ("" + str(temp)))
-  
+    # ส่งค่าอุณหภูมิผ่าน USB Serial
+    serial.write_line("USB Temp:" + str(temp))
+    # ส่งค่าอุณหภูมิผ่าน Bluetooth UART
+    bluetooth.uart_write_line("BLE Temp:" + str(temp))
+    # แสดงผลบนหน้าจอ LED ตามอุณหภูมิ
     if temp <= 25:
         basic.show_leds("""
             . . . . .
@@ -27,6 +30,6 @@ def on_forever():
             """)
     else:
         basic.clear_screen()
-  
-    basic.pause(500)
+    # หน่วงเวลาเพื่อไม่ส่งข้อมูลถี่เกินไป
+    basic.pause(100)
 basic.forever(on_forever)
